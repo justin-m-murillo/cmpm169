5d57a12f5795d8440226c21f03fc2f0f4b9dcbcf
Justin Murillo <murillo_justin@yahoo.com>
2023-01-23 21:32:09 -0800

    finalized experiment

--- experiment2/particle.js
+++ experiment2/particle.js
@@ -1,58 +1,63 @@
-function Particle() {
-    this.pos = createVector(random(width), random(height));
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxSpeed = 4;
+// The Particle class used to paint strokes on the Canvas based on direction and magnitude
+// provided by the flow field
 
-    this.prevPos = this.pos.copy();
+class Particle {
+    constructor(width, height) {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        this.maxSpeed = 4;
 
-    this.update = function() {
-        this.vel.add(this.acc);
-        this.vel.limit(this.maxSpeed);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
-    }
+        this.prevPos = this.pos.copy();
 
-    this.follow = function(vectors) {
-        let x = floor(this.pos.x / scl);
-        let y = floor(this.pos.y / scl);
-        let index = x + y * cols;
-        let force = vectors[index];
-        this.applyForce(force);
-    }
+        this.update = function () {
+            this.vel.add(this.acc);
+            this.vel.limit(this.maxSpeed);
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+        };
 
-    this.applyForce = function(force) {
-        this.acc.add(force);
-    }
+        this.follow = function (vectors) {
+            let x = floor(this.pos.x / scl);
+            let y = floor(this.pos.y / scl);
+            let index = x + y * cols;
+            let force = vectors[index];
+            this.applyForce(force);
+        };
 
-    this.show = function() {
-        stroke(0, 5);
-        strokeWeight(1);
-        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
-        this.updatePrev();
-    }
-    
-    this.updatePrev = function() {
-        this.prevPos.x = this.pos.x;
-        this.prevPos.y = this.pos.y;
-    }
+        this.applyForce = function (force) {
+            this.acc.add(force);
+        };
 
-    this.edges = function() {
-        if (this.pos.x > width) { 
-            this.pos.x = 0;
+        this.show = function (red, green, blue) {
+            stroke(red, green, blue, 5);
+            strokeWeight(1);
+            line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
             this.updatePrev();
-        }
-        if (this.pos.x < 0) {
-            this.pos.x = width;
-            this.updatePrev();
-        }
-        if (this.pos.y > height) {
-            this.pos.y = 0;
-            this.updatePrev();
-        }
-        if (this.pos.y < 0) {
-            this.pos.y = height;
-            this.updatePrev();
-        }
+        };
+
+        this.updatePrev = function () {
+            this.prevPos.x = this.pos.x;
+            this.prevPos.y = this.pos.y;
+        };
+
+        this.edges = function () {
+            if (this.pos.x > width) {
+                this.pos.x = 0;
+                this.updatePrev();
+            }
+            if (this.pos.x < 0) {
+                this.pos.x = width;
+                this.updatePrev();
+            }
+            if (this.pos.y > height) {
+                this.pos.y = 0;
+                this.updatePrev();
+            }
+            if (this.pos.y < 0) {
+                this.pos.y = height;
+                this.updatePrev();
+            }
+        };
     }
 }
--- experiment2/sketch.js
+++ experiment2/sketch.js
@@ -1,11 +1,20 @@
-p5.disableFriendlyErrors = true; // disables FES
+// Justin Murillo - CMPM 169
+// Jan 2023
+//
+// Based on The Coding Train's Coding Challenge #24: Perlin Noise Flow Field
+// https://www.youtube.com/watch?v=BjoM9oKOAKY&list=PLRqwX-V7Uu6bgPNQAdxQZpJuJCjeOr7VD&index=6&ab_channel=TheCodingTrain
+//  
+// Code was revised to include interactive components that were not discussed in the video above
+// along with a few modifications to demonstrate understanding of the project
 
+p5.disableFriendlyErrors = true; // disables FES
 
 let inc = 0.1;
 let scl = 10;
 let cols, rows;
 
 let zoff = 0;
+let zInc;
 
 let fr;
 
@@ -13,55 +22,146 @@ let particles = [];
 
 let flowField;
 
-function setup() {
-  createCanvas(600, 600);
-  fr = createP('');
-  
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-  
+// random seed
+let seed;
 
+// booleans
+let showLines = false;
+let toClear = false;
+let toFreeze = false;
+
+// getters for sliders
+let rVal;
+let gVal;
+let bVal;
+let zVal;
+let pVal;
+
+// size of the flow field
+let xField;
+let yField;
+
+function setup() {
+  let canvas = createCanvas(610, 710);
+  canvas.parent('sketch');
+
+  xField = width-10;
+  yField = height-110;
+
+  // gather UI data
+  rVal = select('#rSlider');
+  gVal = select('#gSlider');
+  bVal = select('#bSlider');
+  zVal = select('#zSlider');
+  pVal = select('#pSlider');
+  
+  // setup drawing space
+  cols = floor((xField) / scl);
+  rows = floor((yField) / scl);
+
+  // declare flow field for storing vector data of each cell
   flowField = new Array(cols * rows);
 
-  for (let i = 0; i < 1000; i++) {
-    particles[i] = new Particle();
-  }
+  // generate particles
+  generateParticles();
 }
 
 function draw() {
-  //background(255);
-  randomSeed(10);
+
+  // produce background when showing vector lines
+  if (showLines) {
+    background(255);
+  }
+  // add more particles into the system
+  if (pVal.value() > particles.length) {
+    addParticles(pVal.value());
+  }
+  // subtract particles from the system
+  if (pVal.value() < particles.length) {
+    subParticles(pVal.value());
+  }
+
   let yoff = 0;
   for (let y = 0; y < rows; y++) {
     let xoff = 0;
     for (let x = 0; x < cols; x++) {
       let index = x + y * cols;
-      let angle = noise(xoff, yoff, zoff) * TWO_PI * 2; // get a random angle between 0 and 2PI
-      let v = p5.Vector.fromAngle(angle);
-      v.setMag(1);
-      flowField[index] = v;
-      xoff += inc;
-      stroke(0, 50);
-      push();
-
-      translate(x * scl, y * scl);
-      rotate(v.heading());
-      strokeWeight(1);
-      //line(0, 0, scl, 0);
+      let angle = noise(xoff, yoff, zoff) * TWO_PI * 2; // get noise angle
+      let v = p5.Vector.fromAngle(angle); // calculate vector
+      v.setMag(1); // set vector magnitude
+      flowField[index] = v; // assign vector to flow field cell
+      xoff += inc; // increment x-offset
       
-      pop();
+      push(); // preserve current render changes
+      translate(x * scl, y * scl); // position vector lines based on coordinates
+      rotate(v.heading()); // rotate lines based on their heading (vector direction)
+      if (showLines) { // if showLines is toggled, render vector lines
+        stroke(0, 50);
+        strokeWeight(1);
+        line(0, 0, scl, 0);
+      }
+      pop(); // return to main draw
     }
-    yoff += inc;
+    yoff += inc; // increment y-offset
 
-    zoff += 0.0003;
+    zInc = zVal.value()/10000; // calculate z-offset increment based on UI
+    zoff += (zInc); // increment z-offset
   }
 
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].follow(flowField);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
+  if (!showLines) { // if showLines is toggled off, paint strokes onto canvas
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].follow(flowField);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show(rVal.value(), gVal.value(), bVal.value());
+    }
   }
 
-  fr.html(floor(frameRate()));
+  // UI info
+  rectMode(CENTER);
+  noStroke();
+  rect(width / 2, height-60, width + 100, 50);
+  let str = `RGB (${rVal.value()}, ${gVal.value()}, ${bVal.value()})    Noise: ${zInc}    Particles: ${particles.length}`;
+  textSize(18);
+  text(str, 0, height-50);
+}
+
+function generateParticles() {
+  for (let i = 0; i < pVal.value(); i++) {
+    particles[i] = new Particle(xField, yField);
+  }
+}
+
+function addParticles() {
+  for (let i = particles.length; i < pVal.value(); i++) {
+    particles[i] = new Particle(xField, yField);
+  }
+}
+
+function subParticles() {
+  let newCount = particles.length - pVal.value();
+  particles.splice(0, newCount);
+}
+
+function toggleLines() {
+  clear();
+  showLines = !showLines;
+}
+
+function clearField() {
+  let newSeed = floor(random(10000));
+  noiseSeed(newSeed); // generate new noise seed
+  zoff = 0;
+  clear();
+}
+
+function freeze() {
+  if (!toFreeze) {
+    noLoop();
+  }
+  else {
+    loop();
+  }
+
+  toFreeze = !toFreeze;
 }
